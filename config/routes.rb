f225d97c0df224f684253ad2fc8b92554f2eb40f
andre the <andre.the12@gmail.com>
2017-03-02 19:26:57 +0800

    added routes and controllers for parks

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'parks'
+  get 'parks/1'
+
+  resources :parks
+
 end
