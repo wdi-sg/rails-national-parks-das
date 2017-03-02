7b93935ff31a9aa08076dd367a4449824c3bd45e
Heng Desmond <desmond@dsrecruitment.sg>
2017-03-02 19:33:43 +0800

    Add Home Index

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,12 @@ Rails.application.routes.draw do
   get 'home/index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get 'parks'
-  get 'parks/1'
+  # get 'parks'
+  # get 'parks/1'
 
   resources :parks
+
+  root 'home#index'
+
 
 end
