197705b75ce6d9f4b5ffb75c12dea2e6925f2f7c
andre the <andre.the12@gmail.com>
2017-03-03 12:38:48 +0800

    added new files

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
+  get 'parks/new'
+
+  get 'parks/create'
+
+  get 'parks/edit'
+
+  get 'parks/update'
+
+  get 'parks/destroy'
+
   get 'home/index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+5.times do
+ Park.create(name: Faker::Pokemon.location , description: 'National Park in CA' , picture: Faker::Placeholdit.image("50x50", 'jpg'))
+end
