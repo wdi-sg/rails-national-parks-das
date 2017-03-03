f1f9a1ccdcb801cc826ebc2fbab7d8849c3d5657
andre the <andre.the12@gmail.com>
2017-03-03 15:26:33 +0800

    new form page for new park

--- app/controllers/parks_controller.rb
+++ app/controllers/parks_controller.rb
@@ -10,16 +10,29 @@ class ParksController < ApplicationController
 
   def show
     @parks = Park.find(params[:id])
+
+    respond_to do |format|
+      format.html
+      format.json { render json: @park }
+    end
   end
+
+  def new
+    @new_park=Park.new
+  end
+
   def create
 
   end
+
   def edit
 
   end
+
   def update
 
   end
+
   def destroy
 
   end
