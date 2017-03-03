1686b02fdb8a043bebd67c53a6cbf949c5a996ab
andre the <andre.the12@gmail.com>
2017-03-03 14:37:01 +0800

    added scss files and links

--- app/controllers/parks_controller.rb
+++ app/controllers/parks_controller.rb
@@ -1,9 +1,26 @@
 class ParksController < ApplicationController
   def index
     @parks = Park.all
+
+    respond_to do |format|
+      format.html
+      format.json{render json: @parks}
+    end
   end
 
   def show
     @parks = Park.find(params[:id])
+  end
+  def create
+
+  end
+  def edit
+
+  end
+  def update
+
+  end
+  def destroy
+
   end
 end
